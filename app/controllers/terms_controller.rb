8bb4a3768cd7f4c408c427ca37d3f03375840836
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-13 16:52:37 -0500

    add controller term

---
+++ app/controllers/terms_controller.rb
@@ -0,0 +1,6 @@
+class TermsController < ApplicationController
+  def index 
+    @terms = Term.all
+    render json: @terms 
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Term.create(word: 'apple', definition:'a red fruits')
+Term.create(word: 'pears', definition:'a red fruits')
